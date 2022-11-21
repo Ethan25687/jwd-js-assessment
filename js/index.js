7297e2fb8a311c1b156ac5ac8432415150a0c79f
Ethan <ethan.stuchlik@yahoo.com>
2022-11-22 10:45:16 +1100

    Reset button function added

--- js/index.js
+++ js/index.js
@@ -10,9 +10,9 @@
       1. Calculate the score as the total of the number of correct answers
 
       2. Add an Event listener for the submit button, which will display the score and highlight 
-         the correct answers when the button is clicked. Use the code from lines 67 to 86 to help you.
+         the correct answers when the button is clicked. Use the code from lines 77 to 96 to help you.
 
-      3. Add 2 more questions to the app (each question must have 4 options).
+      3. Add 2 more questions to the app (each question must have 4 options). ✔
 
       4. Reload the page when the reset button is clicked (hint: search window.location)
 
@@ -95,6 +95,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  // call the displayQuiz function
   displayQuiz();
 });
+
+document.getElementById(btnReset).onclick = window.location;
